0b8acd545cb3dd733e58fafdffaf09658e5ae5a0
martin <martinduffy@hotmail.co.uk>
2023-10-24 18:33:34 +0100

    added Question  Object/Class
    added Quiz class not finished

---
+++ Question.cs
@@ -0,0 +1,9 @@
+﻿
+[Serializable]
+public class Question
+{
+    public string Query { get; set; }
+    public List<string> Choices{ get; set; } = new List<string>();
+    public List<int> Answers { get; set; } = new List<int>();
+}
+
---
+++ Quiz.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class Quiz
+{
+    public List<Question> Questions { get; set; } = new List<Question>();
+
+    public void AddQuestion(Question question)
+    {
+        Questions.Add(question);
+    }
+
+    public void SaveToFile(string path)
+    {
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write);
+        formatter.Serialize(stream, this);
+        stream.Close();
+    }
+
+    public static Quiz LoadFromFile(string path)
+    {
+        IFormatter formatter = new BinaryFormatter();
+        Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        Quiz quiz = (Quiz)formatter.Deserialize(stream);
+        stream.Close();
+        return quiz;
+    }
+}
