11428904475a19f9750a6d90e21d34a697c84063
martin <martinduffy@hotmail.co.uk>
2023-11-16 20:06:35 +0000

    add comment to Program class and made query private in Question class

--- Question.cs
+++ Question.cs
@@ -1,7 +1,7 @@
 ﻿[Serializable]
 public class Question
 {
-    public string Query { get; set; }
+    private string Query { get; set; }
     public List<string> Choices{ get; set; } = new List<string>();
     public List<string> Answers { get; set; } = new List<string>();
 
