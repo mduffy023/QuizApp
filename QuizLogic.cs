e94c3f2a7f6f8388bc869c354e8d4ebf058358de
martin <martinduffy@hotmail.co.uk>
2023-11-16 19:57:44 +0000

    add new method to Quizlogic

--- QuizLogic.cs
+++ QuizLogic.cs
@@ -1,4 +1,17 @@
 ﻿public class QuizLogic
 {
+    private Quiz quiz;
+    private FileOperations fileHandler;
+
+    /// <summary>
+    /// Initializes a new Instance of the Quizlogic Class 
+    /// </summary>
+    /// <param name="quiz">the quiz to manage</param>
+    /// <param name="fileHandler">the file handler for saving and loading the quiz</param>
+    public QuizLogic(Quiz quiz, FileOperations fileHandler)
+    {
+        this.quiz = quiz;
+        this.fileHandler = fileHandler;
+    }
 
 }
